d478263f7f247b464571719bed49e7527cb31f2a
julien.dubiel@epita.fr <contact@juliendubiel.net>
2015-03-08 22:35:41 +0100

    Moved client.cc in client folder

---
+++ src/client/client.hh
@@ -0,0 +1,34 @@
+#ifndef MP2P_CLIENT
+# define MP2P_CLIENT
+
+# include <libconfig.h++>
+# include <libtools.hh>
+
+using namespace network;
+
+class Client
+{
+  private:
+    std::unique_ptr<libconfig::Config> config_; // FIXME: Useless?
+
+    unsigned port_;
+    std::string host_;
+
+    io_service io_service_; // Does not need instantiation
+    ip::tcp::socket socket_;
+
+    KeepAlive handle(Session& session);
+    void send(Session& session);
+
+  public:
+    Client(std::unique_ptr<libconfig::Config>&& config);
+    ~Client();
+
+    // Creates threads & make them bind the same port defined in config.
+    void run();
+
+    // Causes the server to stop it's running threads if any.
+    void stop();
+};
+
+#endif /* MP2P_CLIENT */
