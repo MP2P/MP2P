4b39f7b5889a0d27367eb70ed4912116cebc3acf
julien.dubiel@epita.fr <contact@juliendubiel.net>
2015-03-09 18:38:00 +0100

    Added file error.cc

---
+++ src/libtools/error.cc
@@ -0,0 +1,52 @@
+#include <libtools.hh>
+
+namespace network
+{
+  Error::Error(const ErrorType et)
+    : status_{et}
+  {}
+  Error::~Error()
+  {}
+
+  Error::ErrorType Error::status_get()
+  {
+    return status_;
+  }
+
+  const std::ostringstream& Error::stream_get() const
+  {
+    return stream_;
+  }
+
+  Error& Error::operator=(Error& e)
+  {
+    status_ = e.status_get();
+    stream_.str(e.stream_get().str());
+    return *this;
+  }
+
+  Error& Error::operator=(ErrorType e)
+  {
+    status_ = e;
+    return *this;
+  }
+
+
+  // Put the parameter in stream_
+  template <typename T> Error& Error::operator<<(const T& t)
+  {
+    stream_ << t;
+    return *this;
+  }
+
+  Error& Error::operator<<(std::ostream& (*f)(std::ostream&))
+  {
+    stream_ << f;
+    return *this;
+  }
+
+  std::ostream& operator<<(std::ostream& o, const Error& e)
+  {
+    return o << e.stream_get().str();
+  }
+}
