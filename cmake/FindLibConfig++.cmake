2e2fc8c6de9e885f9c71140239e73043ebde7a2d
julien.dubiel@epita.fr <contact@juliendubiel.net>
2015-03-15 22:40:08 +0100

    Deleted cmake folder, added libconfig finder to the main CMakeList.txt file

--- cmake/FindLibConfig++.cmake
+++
@@ -1,7 +0,0 @@
-# Find libconfig++
-
-find_path(LIBCONFIG++_INCLUDE_DIR libconfig.h++)
-find_library(LIBCONFIG++_LIBRARY config++)
-if (LIBCONFIG++_INCLUDE_DIR AND LIBCONFIG++_LIBRARY)
-   set(LIBCONFIG++_FOUND TRUE)
-endif (LIBCONFIG++_INCLUDE_DIR AND LIBCONFIG++_LIBRARY)
