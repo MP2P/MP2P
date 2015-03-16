36d8edb7ad1ec5bf9189b8169bb5454d6fcbf500
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2015-03-16 05:47:10 +0100

    Add OpenSSL and hashing function

--- src/libtools/files/files.cc
+++ src/libtools/files/files.cc
@@ -1,5 +1,22 @@
+#include <iomanip>
+#include <sstream>
+#include <openssl/sha.h>
+
 #include <files.hh>
+
 
 namespace files
 {
+  // SHA-1 hash a buffer of bytes
+  std::string hash_buffer(const unsigned char* buff, size_t size)
+  {
+    unsigned char hash[20];
+    SHA256(buff, size, hash);
+    std::stringstream result;
+    for (int i = 0; i < 20; ++i)
+      result << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+
+    return result.str();
+  }
+
 }
--- src/libtools/files/files.hh
+++ src/libtools/files/files.hh
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <string>
+
 namespace files
 {
-
+  std::string hash_buffer(const unsigned char* buff, size_t size);
 }
