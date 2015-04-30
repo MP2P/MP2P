62ea72e9df22639318a16551372ad51b16260572
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2015-04-30 12:45:33 +0200

    Update shared-buffer documentation

--- src/libtools/utils/shared-buffer.hh
+++ src/libtools/utils/shared-buffer.hh
@@ -4,27 +4,45 @@
 
 namespace utils
 {
+  /* A shared buffer is a buffer containing a vector as a container.
+   * It should be used to be copied around and shared between multiple objects
+   * and operations.
+   */
   class shared_buffer
   {
     public:
+      using container_type = std::vector<char>;
+
+      // Empty buffer with a preallocated size
+      shared_buffer(size_t size);
+
+      // Buffer with a preallocated container
+      shared_buffer(const std::shared_ptr<container_type>& data);
+
+      // Create a buffer by moving a container inside the old one
+      shared_buffer(container_type&& data);
+
+      // Copy data from a char buffer to the container
+      shared_buffer(const char* data, size_t size);
+
+      // MutableBufferSequence requirements
+
       using value_type = boost::asio::mutable_buffer;
       using const_iterator = const boost::asio::mutable_buffer*;
-
-      shared_buffer(size_t size);
-      shared_buffer(const std::shared_ptr<std::vector<char>>& data);
-      shared_buffer(std::vector<char>&& data);
-      shared_buffer(const char* data, size_t size);
 
       const_iterator begin() const;
       const_iterator end() const;
 
+      // Accessors
       const value_type buffer_get() const;
-      const std::vector<char>& data_get() const;
+      const container_type& data_get() const; // FIXME : Don't make this public
 
+      // Create a string using the data
+      // Testing purpose only
       const std::string string_get() const;
 
     private:
-      std::shared_ptr<std::vector<char>> data_;
+      std::shared_ptr<container_type> data_;
       boost::asio::mutable_buffer buffer_;
   };
 }
--- src/libtools/utils/shared-buffer.hxx
+++ src/libtools/utils/shared-buffer.hxx
@@ -3,26 +3,26 @@
 namespace utils
 {
   inline shared_buffer::shared_buffer(size_t size)
-    : data_{std::make_shared<std::vector<char>>(size, '\0')},
+    : data_{std::make_shared<container_type>(size, '\0')},
       buffer_{&*data_->begin(), size}
   {
   }
 
-  inline shared_buffer::shared_buffer(const std::shared_ptr<std::vector<char>>& data)
+  inline shared_buffer::shared_buffer(const std::shared_ptr<container_type>& data)
     : data_{data},
       buffer_{&*data_->begin(), data->size()}
   {
   }
 
-  inline shared_buffer::shared_buffer(std::vector<char>&& data)
-    : data_{std::make_shared<std::vector<char>>(
-        std::forward<std::vector<char>>(data))},
+  inline shared_buffer::shared_buffer(container_type&& data)
+    : data_{std::make_shared<container_type>(
+        std::forward<container_type>(data))},
       buffer_{&*data_->begin(), data.size()}
   {
   }
 
   inline shared_buffer::shared_buffer(const char* data, size_t size)
-    : data_{std::make_shared<std::vector<char>>(size, '\0')},
+    : data_{std::make_shared<container_type>(size, '\0')},
       buffer_{&*data_->begin(), size}
   {
     memcpy(&*data_->begin(), data, size);
@@ -44,7 +44,8 @@ namespace utils
     return buffer_;
   }
 
-  inline const std::vector<char>& shared_buffer::data_get() const
+  inline const shared_buffer::container_type&
+  shared_buffer::data_get() const
   {
     return *data_;
   }
