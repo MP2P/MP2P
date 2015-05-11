1761e7429090ee507d6cb941db618c096f94ea8e
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2015-05-11 12:47:34 +0200

    Revert "Add shared_buffer tests"
    This reverts commit 29c8cf4cb61911ea79b51464502def37d81225b9.

--- src/tests/shared-buffer/main.cc
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-
-#include "../catch.hh"
-#include "../../libtools/utils/shared-buffer.hh"
-
-TEST_CASE("Constructors", "[constructions]")
-{
-  utils::shared_buffer(0);
-}
