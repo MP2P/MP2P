d8870e2baf651afe6c01e088add2d3e47ebeb4cb
dubrzr <contact@juliendubiel.net>
2015-06-06 20:41:14 +0200

    Fix style.

--- src/master/database_items.hxx
+++ src/master/database_items.hxx
@@ -1,88 +1,131 @@
 namespace DB
 {
-  inline fsize_type FileItem::file_size_get() const
+  inline
+  fsize_type
+  FileItem::file_size_get() const
   {
     return file_size_;
   }
 
-  inline rdcy_type FileItem::redundancy_get() const
+  inline
+  rdcy_type
+  FileItem::redundancy_get() const
   {
     return redundancy_;
   }
 
-  inline rdcy_type FileItem::current_redundancy_get() const
+  inline
+  rdcy_type
+  FileItem::current_redundancy_get() const
   {
     return current_redundancy_;
   }
 
-  inline fid_type FileItem::id_get() const
+  inline
+  fid_type
+  FileItem::id_get() const
   {
     return id_;
   }
 
-  inline std::string FileItem::name_get() const
+  inline
+  std::string
+  FileItem::name_get() const
   {
     return name_;
   }
 
-  inline CharT* FileItem::hash_get()
+  inline
+  CharT*
+  FileItem::hash_get()
   {
     return &hash_[0];
   }
 
-  inline bool FileItem::is_replicated() const
+  inline
+  bool
+  FileItem::is_replicated() const
   {
     return current_redundancy_ == redundancy_;
   }
 
-  inline bool FileItem::is_uploaded() const
+  inline
+  bool
+  FileItem::is_uploaded() const
   {
     return uploaded_;
   }
 
-  inline fid_type PartItem::fid_get() const
+  inline
+  fid_type
+  PartItem::fid_get() const
   {
     return partid_.fid;
   }
 
-  inline partnum_type PartItem::num_get() const
+  inline
+  partnum_type
+  PartItem::num_get() const
   {
     return partid_.partnum;
   }
 
-  inline CharT* PartItem::hash_get()
+  inline
+  CharT*
+  PartItem::hash_get()
   {
     return &hash_[0];
   }
 
-  inline std::vector<stid_type> PartItem::locations_get() const
+  inline
+  std::vector<stid_type>
+  PartItem::locations_get() const
   {
     return locations_;
   }
 
-  inline mtid_type MasterItem::id_get() const
+  inline
+  mtid_type
+  MasterItem::id_get() const
   {
     return id_;
   }
 
-  inline std::string MasterItem::host_addr_get() const
+  inline
+  std::string
+  MasterItem::host_addr_get() const
   {
     return host_addr_;
   }
 
-  inline stid_type StorageItem::id_get() const
+  inline
+  stid_type
+  StorageItem::id_get() const
   {
     return id_;
   }
 
-  inline std::string StorageItem::host_addr_get() const
+  inline
+  std::string
+  StorageItem::host_addr_get() const
   {
     return host_addr_;
   }
-  std::string port_get() const;
-  ADDR addr_get() const;
 
-  inline avspace_type StorageItem::available_space_get() const
+  inline
+  std::string
+  port_get() const
+  {
+
+  }
+  ADDR addr_get() const
+  {
+
+  }
+
+  inline
+  avspace_type
+  StorageItem::available_space_get() const
   {
     return available_space_;
   }
