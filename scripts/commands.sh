9d18d7d508da8755df32f38e707417074acca68f
dubrzr <contact@juliendubiel.net>
2015-07-01 16:25:31 +0200

    Added the 2 other RPs, and a script to reset the id and the stored files.

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -7,21 +7,29 @@
 IP0="192.168.1.11"
 IP1="192.168.1.12"
 IP2="192.168.1.13"
+IP3="192.168.1.15"
+IP4="192.168.1.22"
 
 PORT0="22"
 PORT1="22"
 PORT2="22"
+PORT3="22"
+PORT4="22"
 
 read -s -p "Please enter your MP2P password:" PWD
 echo
-#if [[ $(whoami) != "rusty" ]]; then
+if [[ $(whoami) != "dsensei" ]]; then
   IP0="rpi.juliendubiel.net"
   IP1="rpi.juliendubiel.net"
   IP2="rpi.juliendubiel.net"
+  IP3="rpi.juliendubiel.net"
+  IP4="rpi.juliendubiel.net"
   PORT0="4201"
   PORT1="4202"
   PORT2="4203"
-#fi
+  PORT3="4202"
+  PORT4="4203"
+fi
 
 if [ "$#" -eq 2 ] && [ -f "$1" ]; then
   echo Trying to connect to Raspberry Py 0...
@@ -32,6 +40,12 @@ if [ "$#" -eq 2 ] && [ -f "$1" ]; then
   echo
   echo Trying to connect to Raspberry Py 2...
   sshpass -p $PWD ssh root@$IP2 -p $PORT2 -T 'sh -s' < $1
+  echo
+  echo Trying to connect to Raspberry Py 3...
+  sshpass -p $PWD ssh root@$IP0 -p $PORT3 -T 'sh -s' < $1 &
+  echo
+  echo Trying to connect to Raspberry Py 4...
+  sshpass -p $PWD ssh root@$IP4 -p $PORT4 -T 'sh -s' < $1 &
 else
   if [ "$#" -eq 1 ]; then
     if [ -f "$1" ]; then
@@ -43,6 +57,12 @@ else
       echo
       echo Trying to connect to Raspberry Py 2...
       sshpass -p $PWD ssh root@$IP2 -p $PORT2 -T 'sh -s' < $1
+      echo
+      echo Trying to connect to Raspberry Py 3...
+      sshpass -p $PWD ssh root@$IP3 -p $PORT3 -T 'sh -s' < $1
+      echo
+      echo Trying to connect to Raspberry Py 4...
+      sshpass -p $PWD ssh root@$IP4 -p $PORT4 -T 'sh -s' < $1
     else
       echo Trying to connect to Raspberry Py 0...
       sshpass -p $PWD ssh root@$IP0 -p $PORT0 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list &
@@ -52,6 +72,24 @@ else
       echo
       echo Trying to connect to Raspberry Py 2...
       sshpass -p $PWD ssh root@$IP2 -p $PORT2 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
+      echo
+      echo Trying to connect to Raspberry Py 3...
+      sshpass -p $PWD ssh root@$IP3 -p $PORT3 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list &
+      echo
+      echo Trying to connect to Raspberry Py 4...
+      sshpass -p $PWD ssh root@$IP4 -p $PORT4 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
+      echo
+      echo Trying to connect to Raspberry Py 1...
+      sshpass -p $PWD ssh root@$IP1 -p $PORT1 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list &
+      echo
+      echo Trying to connect to Raspberry Py 2...
+      sshpass -p $PWD ssh root@$IP2 -p $PORT2 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
+      echo
+      echo Trying to connect to Raspberry Py 3...
+      sshpass -p $PWD ssh root@$IP3 -p $PORT3 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list &
+      echo
+      echo Trying to connect to Raspberry Py 4...
+      sshpass -p $PWD ssh root@$IP4 -p $PORT4 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
     fi
   else
     echo Trying to connect to Raspberry Py 0...
@@ -62,6 +100,12 @@ else
     echo
     echo Trying to connect to Raspberry Py 2...
     sshpass -p $PWD ssh root@$IP2 -p $PORT2 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
+    echo
+    echo Trying to connect to Raspberry Py 3...
+    sshpass -p $PWD ssh root@$IP3 -p $PORT3 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
+    echo
+    echo Trying to connect to Raspberry Py 4...
+    sshpass -p $PWD ssh root@$IP4 -p $PORT4 -T 'sh -s' < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/command_list
   fi
 fi
 echo DONE
